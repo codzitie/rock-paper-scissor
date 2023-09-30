cc9bf64ad5466707afa005e79af3af5198adf6c3
codzitie <gmkrishnakumar333@gmail.com>
2023-09-30 11:45:42 +0530

    analyzed a lame bug which was a barrier for weeks (if (player || computer === 5 )) and the fixed by (if (player === 5 || computer === 5))

--- myscripts.js
+++ myscripts.js
@@ -1,59 +1,66 @@
 function getComputerChoice(){
     const myArray = ['rock','paper','scissor'];  
     const computer_result = myArray[Math.floor(Math.random() * myArray.length)];  
-    console.log(computer_result);  
+    // console.log(computer_result);  
     return computer_result;
 };
-// getComputerChoice();
 
 function playerSelection(){
     const myArray = ['rock','paper','scissor'];  
     const player_result = myArray[Math.floor(Math.random() * myArray.length)];  
-    console.log(player_result);  
+    // console.log(player_result);  
     return player_result;
 
 };
-// playerSelection();
+
+player = 0;
+computer = 0;
 
 function playRound() {
+        
     player_result = playerSelection()
     computer_result = getComputerChoice()
-    globalThis.player = []
-    globalThis.computer = []
+    console.log('saved player result',player_result)
+    console.log('saved computer result',computer_result)
+    
     if (player_result === "paper" && computer_result === "rock"){
         player+=1;
-    }else {
-        computer+=1;
-    }; 
-    if (player_result === "scissor" && computer_result === "paper"){
+    } 
+    else if (player_result === "scissor" && computer_result === "paper"){
         player+=1;
-    }else {
-        computer+=1;
-    }; 
-    if (player_result === "rock" && computer_result === "scissor"){
+    } 
+    else if (player_result === "rock" && computer_result === "scissor"){
         player+=1;
-    }else {
-        computer+=1;
-    }; 
-    if (player_result === computer_result){
+    } 
+    else if (player_result === computer_result){
         console.log('tie')
-    };
-    
+        playRound();
+    }else{
+        computer+=1;
+    }
   };
 
+
 function game(){
-    playRound();
-    for (let i = 0; i<5; i++){
+    for (i=0; i<100; i++){
         playRound();
+        if (player === 5 || computer === 5){
+            break;
+        };
     };
+
     console.log('You = ',player)
     console.log('Computer = ',computer)
     if (player>computer){
         console.log("you are the winner")
+    }
+    else if (player===computer){
+        console.log('draw')
     }else{
         console.log('You lose')
     }
+
+
 };
 
 game();
-
